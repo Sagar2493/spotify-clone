f0148366fe68a225ff6ae5dddf6348a56b0fde23
Sagar Chowdhury <sagar.bzn@gmail.com>
2025-06-14 03:00:37 +0530

    Updated tsconfig.json , tsconfig.app.json, vite.config.ts and then installed shadcdn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <>
-      <h1 className="text-red-500 text-5xl">Hello</h1>
+      <h1 className="text-5xl text-red-500">Hello</h1>
+      <Button>This is a button</Button>
     </>
   )
 }
